de9a5e424a906d08184d286ba1c8b208700ef03c
Yehor Penshyn <pensinegor@gmail.com>
2022-10-29 20:12:05 +0300

    Smart multiply

---
+++ lecture4/homeTask2/index.js
@@ -0,0 +1,17 @@
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function double(number) {
+    return multiply(number, 2);
+}
+
+function square(number) {
+    return multiply(number, number);
+}
+
+console.log(double(5));
+console.log(square(5));
+
+
+
